eab5a6b526f200ec4acd3b3fef36bd503db77111
Mo Gusbi <mo@Mos-MacBook-Pro.local>
2019-03-18 22:32:48 +0000

    Typography component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,19 @@
 import React, {Fragment} from 'react';
 import Grid from '../components/Grid';
 import GridCol from '../components/GridCol';
+import Typography from '../components/Typography/Typography';
 
 function Index () {
   return (
     <Fragment>
       <Grid>
-        <GridCol>Index</GridCol>
+        <GridCol>
+          <Typography component='h1' variant='h1'>Hello world</Typography>
+
+          <Typography component='p' variant='lead'>This is a dummy homepage</Typography>
+
+          <Typography component='p' variant='p'>More content will follow...</Typography>
+        </GridCol>
       </Grid>
     </Fragment>
   );
