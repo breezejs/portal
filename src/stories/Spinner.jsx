fca86c33b7cfc83258421fde152682d3a12a9874
Mo Gusbi <mo@Mos-MacBook-Pro.local>
2019-03-24 14:59:58 +0000

    Update spinner storybook

--- src/stories/Spinner.jsx
+++ src/stories/Spinner.jsx
@@ -1,3 +1,4 @@
+import {optionsKnob, withKnobs} from '@storybook/addon-knobs';
 import {storiesOf} from '@storybook/react';
 import React from 'react';
 import GlobalStyles from '../components/GlobalStyles';
@@ -5,28 +6,26 @@ import Spinner from '../components/Spinner';
 
 const stories = storiesOf('Spinner', module);
 
+const options = {
+  danger: 'danger',
+  info: 'info',
+  primary: 'primary',
+  secondary: 'secondary',
+  success: 'success',
+  warning: 'warning'
+};
+const optionsObj = {
+  display: 'select'
+};
+
 stories
+  .addDecorator(withKnobs)
   .addDecorator((storyFn) => (
     <div>
       <GlobalStyles />
       {storyFn()}
     </div>
   ))
-  .add('with primary theme', () => (
-    <Spinner theme="primary" />
-  ))
-  .add('with secondary theme', () => (
-    <Spinner theme="secondary" />
-  ))
-  .add('with info theme', () => (
-    <Spinner theme="info" />
-  ))
-  .add('with success theme', () => (
-    <Spinner theme="success" />
-  ))
-  .add('with danger theme', () => (
-    <Spinner theme="danger" />
-  ))
-  .add('with warning theme', () => (
-    <Spinner theme="warning" />
+  .add('with multiple themes', () => (
+    <Spinner theme={optionsKnob('Theme', options, 'info', optionsObj)} />
   ));
